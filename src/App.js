a88671c76a7fb93e6430451d120ec484a9a90fa6
jlee <yeoniee217@gmail.com>
2020-03-28 19:45:40 -0500

    Front-end React Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: []
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('/categories/all_categories').then(response => {
+      return response.json();
+    }).then(json => {
+      this.setState({categories: json});
+      return json;
+    }).then(json => console.log(this.state.categories))
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello World
+      </div>
+    );
+  }
 }
 
 export default App;
