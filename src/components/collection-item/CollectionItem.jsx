f2c009df47f461787c22405136e57042d0ced5fb
jlee72 <yeoniee217@gmail.com>
2020-03-30 21:40:03 -0500

    Added currency format for product's price and added css.

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import './CollectionItem.styles.scss'
 
 const CollectionItem = ({product}) => {
-  const { num, name, price, description, stock_quantity, discount } = product;
-
+  const { num, name, description, price, stock_quantity, discount, imageUrl } = product;
+  let currencyFormat = new Intl.NumberFormat('en-CA', {
+    style: 'currency',
+    currency: 'CAD',
+  });
   return (
-    <div class="col-md-4">
-      <div class="card mt-5 ml-2">
+    <div class="col-md-3">
+      <div class="card mt-5 ml-5" style={{width:"100%"}}>
+        <img className="card-img-top mt-3" src={imageUrl} alt="Product Image" style={{width:"150px", height:"150px", alignSelf:"center"}}/>
         <div class="card-body">
-          <h5 class="card-title">{name}</h5>
-          <p class="card-text">Price: {price}</p>
-          <p class="card-text">Description: {description}</p>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">Stock Quantity: {stock_quantity}</li>
-            <li class="list-group-item">Email:</li>
-          </ul>
-          <a href="#" class="btn btn-primary mt-3">View</a>
+          <h5 class="card-title" style={{height:"100px"}}>{name}</h5>
+          <p class="card-text font-weight-bold">{currencyFormat.format(price/100)}</p>
+          <a href="#" class="btn btn-primary font-weight-bold add-cart-btn"><i class="fa fa-shopping-cart mr-2" aria-hidden="true"></i> Add to Cart</a>
         </div>
       </div>
     </div>
