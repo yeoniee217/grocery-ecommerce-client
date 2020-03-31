a7395a704c43cc526837f8870afa1b448ce1aa59
jlee72 <yeoniee217@gmail.com>
2020-03-30 21:40:51 -0500

    Added key attr to the list item and some styling.

--- src/components/sub-navbar/SubNavBar.jsx
+++ src/components/sub-navbar/SubNavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef  } from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 import './SubNavBar.styles.scss'
@@ -17,10 +17,10 @@ const SubNavBar = ({categories}) => {
   }
 
   return (
-<div class="mt-3">
+<div class="mt-3" style={{marginLeft:"20%"}}>
     <nav class="collectionPage-nav">
-      <button class='navbar-toggler' onMouseOver={handleMouseOver} type="button" data-toggle="collapse" collapse={false} data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <i className="fas fa-bars"></i> <span class="ml-4">All Categories</span>
+      <button class='navbar-toggler' onMouseOver={handleMouseOver} type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <i className="fas fa-bars"></i> <span class="ml-3">All Categories</span>
       </button>
 
       <div class={`collapse navbar-collapse ${toggle}`} onMouseLeave={handleMouseLeave} id="navbarNav">
@@ -28,14 +28,13 @@ const SubNavBar = ({categories}) => {
           {
             categories.length ?
             categories.map(category =>
-              <li>
-                <Link className="nav-link ml-3" to={{ pathname:`/categories/${category.id}` }}>{category.name}</Link>
+              <li key={category.id}>
+                <Link className="nav-link ml-3" to={`/categories/${category.id}`}>{category.name}</Link>
               </li>
             )
             :
             null
           }
-
         </ul>
       </div>
     </nav>
