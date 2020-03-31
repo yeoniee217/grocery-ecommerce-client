fc793e90e9a8c70f3748b08c4b850ef70ef8abd0
jlee72 <yeoniee217@gmail.com>
2020-03-30 21:42:29 -0500

    Created fetch products method and added componentDidUpdate to update the collection page.

--- src/pages/collection/CollectionPage.jsx
+++ src/pages/collection/CollectionPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Pagination from 'react-paginating';
 
 import './CollectionPage.styles.scss';
 import Header from '../../components/header/Header';
@@ -17,21 +18,31 @@ class CollectionPage extends React.Component {
   componentDidMount() {
     const category_id = this.props.match.params.id
 
-    fetch('http://localhost:3000/categories/all_categories').then(response => {
+    fetch('http://localhost:3000/categories/all_categories.json').then(response => {
       return response.json();
     }).then(json => {
       this.setState({categories: json});
       return json;
     }).then(json => console.log(this.state.categories))
-    .catch(error => console.log(error))
+    .catch(error => console.log(error));
 
-    fetch(`http://localhost:3000/products/by_category_id/${category_id}`).then(response => {
+    this.fetchProducts(category_id);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.match.params.id !== prevProps.match.params.id) {
+      this.fetchProducts(this.props.match.params.id);
+    }
+  }
+
+  fetchProducts = (category_id) => {
+    fetch(`http://localhost:3000/products/by_category_id/${category_id}.json`).then(response => {
       return response.json();
     }).then(json => {
       this.setState({products: json});
       return json;
     }).then(json => console.log(this.state.products))
-    .catch(error => console.log(error))
+    .catch(error => console.log(error));
   }
 
   renderProducts = () => {
@@ -43,12 +54,49 @@ class CollectionPage extends React.Component {
 
   render() {
     return (
-      <div class="container">
+      <div class="">
         <Header categories={ this.state.categories } />
         <SubNavBar categories={ this.state.categories } />
-        <div class="row">
-          {this.renderProducts()}
+        <div style={{marginLeft:"10%", display:"flex"}}>
+            <div class="card mt-5" style={{height:"fit-content"}}>
+              <article class="card-group-item">
+                <header class="card-header"><h6 class="title">Sub Categories</h6></header>
+                <div class="filter-content">
+                  <div class="list-group list-group-flush">
+                    <a href="#" class="list-group-item">Cras justo odio <span class="float-right badge badge-light round">142</span> </a>
+                    <a href="#" class="list-group-item">Dapibus ac facilisis  <span class="float-right badge badge-light round">3</span>  </a>
+                    <a href="#" class="list-group-item">Morbi leo risus <span class="float-right badge badge-light round">32</span>  </a>
+                    <a href="#" class="list-group-item">Another item <span class="float-right badge badge-light round">12</span>  </a>
+                  </div>
+                </div>
+              </article>
+              <article class="card-group-item">
+                <header class="card-header card-border-top"><h6 class="title">Color check</h6></header>
+                <div class="filter-content">
+                  <div class="card-body">
+                    <label class="btn btn-danger">
+                      <input class="" type="checkbox" name="myradio" value=""/>
+                      <span class="form-check-label">Red</span>
+                    </label>
+                    <label class="btn btn-success">
+                      <input class="" type="checkbox" name="myradio" value=""/>
+                      <span class="form-check-label">Green</span>
+                    </label>
+                    <label class="btn btn-primary">
+                      <input class="" type="checkbox" name="myradio" value=""/>
+                      <span class="form-check-label">Blue</span>
+                    </label>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div class="row" style={{width:"70%"}}>
+              {this.renderProducts()}
+            </div>
+
         </div>
+
 
       </div>
     );
