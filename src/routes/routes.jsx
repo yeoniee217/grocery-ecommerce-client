acbe2731b42280b54042217ea6cf16c45728e4e8
jlee72 <yeoniee217@gmail.com>
2020-04-12 02:39:46 -0500

    Added routes to pages.

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HomePage from '../pages/homepage/HomePage';
-// import App from '../App';
 import CollectionPage from '../pages/collection/CollectionPage';
+import ItemPage from '../pages/item/ItemPage'
+import SearchResultPage from '../pages/searchResult/SearchResultPage';
 
 export const routes = (
   <Router>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/categories/:id" component={CollectionPage} />
-        {/* <Route exact path="/login" component={Login} />
-        <Route exact path="/home" component={Home} />
+        <Route exact path="/products/:id" component={ItemPage} />
+        <Route exact path="/search" component={SearchResultPage} />
+        {/* <Route exact path="/login" component={LoginPage} />
         <Route exact path='/asset-management' component={AssetManagementTable} />
         <Route exact path='/create-procedure' component={CreateProcedure} /> */}
       </Switch>
