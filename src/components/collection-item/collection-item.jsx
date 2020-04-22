c8a094b56ba2cac10aa9b13642e0fc8c0b0a30dc
jlee72 <yeoniee217@gmail.com>
2020-04-21 19:02:18 -0500

    Added Add to cart, In cart buttons and removed currency format method in the file

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import {formatCurrency} from '../../util/util'
 import './collection-item.styles.scss'
 
-const CollectionItem = ({product}) => {
+const CollectionItem = ({product, handleAddToCart}) => {
   const { id, num, name, description, price, stock_quantity, discount, imageUrl } = product;
-  let currencyFormat = new Intl.NumberFormat('en-CA', {
-    style: 'currency',
-    currency: 'CAD',
-  });
+
   return (
     <div class="col-md-3">
         <div class="card mt-5 ml-5" style={{width:"100%"}}>
@@ -17,8 +15,19 @@ const CollectionItem = ({product}) => {
             <Link to={{ pathname:`/products/${id}` }} style={{textDecoration: "none", color:"inherit"}}>
               <h5 class="card-title" style={{height:"100px"}}>{name}</h5>
             </Link>
-            <p class="card-text font-weight-bold">{currencyFormat.format(price/100)}</p>
-            <a href="#" class="btn btn-primary font-weight-bold add-cart-btn"><i class="fa fa-shopping-cart mr-2" aria-hidden="true"></i> Add to Cart</a>
+            <p class="card-text font-weight-bold">{formatCurrency(price/100)}</p>
+            {
+              !product.alreadyInCart ?
+                <button type="button" class="btn btn-primary font-weight-bold add-cart-btn"
+                  onClick={e => handleAddToCart(e, product)}>
+                  <i class="fa fa-shopping-cart mr-2" aria-hidden="true"></i> Add to Cart
+                </button>
+              :
+                <button type="button" disabled class="btn btn-success font-weight-bold add-cart-btn"
+                  onClick={e => handleAddToCart(e, product)}>
+                  <i class="fas fa-check mr-2" aria-hidden="true"></i> In Cart
+                </button>
+            }
           </div>
         </div>
     </div>
